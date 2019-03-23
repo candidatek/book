814cf194f33304176dd4c43ebbbec19c1fd8d01a
skshriharik <39544218+skshriharik@users.noreply.github.com>
2019-03-23 19:45:21 +0530

    Add files via upload

--- rest.py
+++ rest.py
@@ -17,18 +17,31 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
+app.debug = True
 mysql = MySQL(app)
 
 
 
 @app.route('/')
 def first():
-    return render_template('rest.')
+    return render_template('rest.html')
 
 @app.route('/rest' , methods=['GET' , 'POST'])
 def rest():
-    searchq = request.args.get('search')
-    return searchq
+    if request.method == 'POST' :
+        userDetais = request.form
+        name = userDetais['search']
+
+    key = request.args.get('search')
+    cur = mysql.connection.cursor()
+    print(key)
+    cur.execute("SELECT DISTINCT title , author FROM books WHERE title LIKE %s OR author LIKE %s  LIMIT 5", ("%" + key + "%", "%" + key + "%"))
+    #cur.execute("SELECT * FROM books WHERE title LIKE '%s'% " ,[key])
+    res = cur.fetchall()
+    #print(res)
+    jsonResult = jsonify(res)
+
+    return jsonify(res)
 
 
 
